b7dbd7a211a7df3898fbd029f6dc167f2f7504e6
wei_glo <wei_glo@123.com>
2020-01-07 14:31:18 +0800

    Test HotFix Branch

--- src/main/java/com/bj/StudyGit.java
+++ src/main/java/com/bj/StudyGit.java
@@ -4,5 +4,6 @@ public class StudyGit {
 	public static void main(String[] args) {
 		System.out.println("StudyGit...Left");
 		System.out.println("StudyGit...Right");
+		System.out.println("StudyGit...HotFix");
 	}
 }
